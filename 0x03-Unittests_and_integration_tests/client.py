5d985bbc6902691d274d4011a7edd7a0af45c874
Hanan Mohamud <hananmohamudy@gmail.com>
2025-06-01 19:21:03 +0300

    updated test_client.py

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -51,8 +51,7 @@ class GithubOrgClient:
     def has_license(repo: Dict[str, Dict], license_key: str) -> bool:
         """Static: has_license"""
         assert license_key is not None, "license_key cannot be None"
-        try:
-            has_license = access_nested_map(repo, ("license", "key")) == license_key
-        except KeyError:
+        license_info = repo.get("license")
+        if not isinstance(license_info, dict):
             return False
-        return has_license
+        return license_info.get("key") == license_key
