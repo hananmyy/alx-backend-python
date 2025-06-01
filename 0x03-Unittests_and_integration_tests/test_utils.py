5434e22bfae18a2be019eaf35464aa5b196f7fda
Hanan Mohamud <hananmohamudy@gmail.com>
2025-06-01 17:18:32 +0300

    updated test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -67,11 +67,12 @@ class TestMemoize(unittest.TestCase):
         """Test that a_method is only called once due to memoization."""
         with patch.object(
             self.TestClass, "a_method", return_value=42
-            ) as mock_method:
+        ) as mock_method:
             test_obj = self.TestClass()
             self.assertEqual(test_obj.a_property, 42)
             self.assertEqual(test_obj.a_property, 42)
             mock_method.assert_called_once()
+
 
 
 class TestGithubOrgClient(unittest.TestCase):
