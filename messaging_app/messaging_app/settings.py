3e582e6c3eb7caf6cf128b29aef00d39fbc993fd
Hanan Mohamud <hananmohamudy@gmail.com>
2025-06-21 20:04:38 +0300

    completed containerization with docker project

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,7 +42,6 @@ INSTALLED_APPS = [
     'chats',
     'django_filters',
     'rest_framework_simplejwt',
-    'rest_framework.authentication.BasicAuthentication',
 ]
 
 MIDDLEWARE = [
@@ -78,12 +78,15 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+      'ENGINE': 'django.db.backends.mysql',
+      'NAME': os.getenv("MYSQL_DB"),
+      'USER': os.getenv("MYSQL_USER"),
+      'PASSWORD': os.getenv("MYSQL_PASSWORD"),
+      'HOST': 'db',
+      'PORT': '3306',
+  }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
